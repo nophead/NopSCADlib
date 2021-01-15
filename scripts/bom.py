7f9122ae662ab0bd660e5c2f5ab6c507797030da
Chris Palmer <nop.head@gmail.com>
2021-01-15 18:13:20 +0000

    Added --hardwarnings to openscad.py

--- scripts/bom.py
+++ scripts/bom.py
@@ -249,7 +249,7 @@ def boms(target = None, assembly = None):
         #
         # Run openscad
         #
-        openscad.run("-D", "$bom=2", "-D", "$preview=true", "--hardwarnings", "-o", "openscad.echo", "-d", bom_dir + "/bom.deps", bom_maker_name)
+        openscad.run("-D", "$bom=2", "-D", "$preview=true", "-o", "openscad.echo", "-d", bom_dir + "/bom.deps", bom_maker_name)
         os.remove(bom_maker_name)
         print("Generating bom ...", end=" ")
 
--- scripts/openscad.py
+++ scripts/openscad.py
@@ -25,7 +25,7 @@ from __future__ import print_function
 import subprocess, sys
 
 def run_list(args, silent = False, verbose = False):
-    cmd = ["openscad"] + args
+    cmd = ["openscad", "--hardwarnings"] + args
     if not silent:
         for arg in cmd:
             print(arg, end=" ")
